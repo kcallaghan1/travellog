2c03e1bf95c1dd529d04c7b33df52ad0bdb4f6da
Danny <dxumail.dxu@gmail.com>
2021-03-29 16:38:06 -0400

    added TravelLog and Location class + test files

---
+++ src/main/java/edu/ithaca/dragon/bank/TravelLog.java
@@ -0,0 +1,10 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.ArrayList;
+
+public class TravelLog {
+    
+    private String title;
+    private String description;
+    private ArrayList<Location> places;
+}
