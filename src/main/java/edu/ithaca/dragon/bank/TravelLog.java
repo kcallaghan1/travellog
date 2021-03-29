6c072d764429d751937c46677340a1f437e1d457
Danny <dxumail.dxu@gmail.com>
2021-03-29 17:11:54 -0400

    added exception throws to constructor and gets

--- src/main/java/edu/ithaca/dragon/bank/TravelLog.java
+++ src/main/java/edu/ithaca/dragon/bank/TravelLog.java
@@ -9,8 +9,23 @@ public class TravelLog {
     private ArrayList<Location> places;
 
     public TravelLog(String title, String description){
-        this.title = title;
-        this.description = description;
-        places = new ArrayList<Location>();
+        if (title.length() > 0){
+            if (description.length() > 0){
+                this.title = title;
+                this.description = description;
+                places = new ArrayList<Location>();
+            }
+            else throw new IllegalArgumentException("The description for this travel log has been left empty.");
+        }
+        else throw new IllegalArgumentException("The title of this travel log has been left empty.");
     }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
 }
