159872695c5a552cdd3915da835e6ae2d76d44d4
Danny <dxumail.dxu@gmail.com>
2021-03-29 22:19:16 -0400

    added implementation for location class constructor and gets

--- src/main/java/edu/ithaca/dragon/bank/Location.java
+++ src/main/java/edu/ithaca/dragon/bank/Location.java
@@ -1,5 +1,30 @@
 package edu.ithaca.dragon.bank;
 
+import java.util.ArrayList;
+
 public class Location {
-    
+
+    private String name;
+    private String address;
+    private ArrayList<String> categories;
+
+    public Location(String name, String address){
+        if (name.length() > 0){
+            if (address.length() > 0){
+                this.name = name;
+                this.address = address;
+                categories = new ArrayList<>();
+            }
+            else throw new IllegalArgumentException("The name for this location has been left empty.");
+        }
+        else throw new IllegalArgumentException("The address of this location has been left empty.");
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
 }
--- src/test/java/edu/ithaca/dragon/bank/LocationTest.java
+++ src/test/java/edu/ithaca/dragon/bank/LocationTest.java
@@ -10,10 +10,10 @@ public class LocationTest {
     void constructorTest(){
         Location location = new Location("Waffle Frolic", "146 E State St, Ithaca, NY 14850");
 
-        assertEquals("Waffle Frolic", location.getName);
-        assertEquals("146 E State St, Ithaca, NY 14850", location.getAddress);
+        assertEquals("Waffle Frolic", location.getName());
+        assertEquals("146 E State St, Ithaca, NY 14850", location.getAddress());
         //check for exception thrown correctly
-        assertThrows(IllegalArgumentException.class, () -> new location("", "146 E State St, Ithaca, NY 14850"));
-        assertThrows(IllegalArgumentException.class, () -> new location("Waffle Frolic", ""));
+        assertThrows(IllegalArgumentException.class, () -> new Location("", "146 E State St, Ithaca, NY 14850"));
+        assertThrows(IllegalArgumentException.class, () -> new Location("Waffle Frolic", ""));
     }
 }
