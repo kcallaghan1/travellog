ed30b7b3496f6d5b557c83a0d1096b36543d9eba
hoshamania <57670943+hoshamania@users.noreply.github.com>
2021-04-01 19:27:54 -0400

    Made minor changes to library regarding adding accounts

--- src/main/java/edu/ithaca/dragon/traveltracker/Library.java
+++ src/main/java/edu/ithaca/dragon/traveltracker/Library.java
@@ -8,6 +8,24 @@ public class Library {
     ArrayList<Location> locationList;
     ArrayList<Account> accountList;
 
+    void addAccount(Account a){
+        accountList.add(a);
+    }
+
+    void addAccount(String username, String email, String password){
+        accountList.add(new Account(username, email, password));
+    }
+
+    void deleteAccount(int i){
+        accountList.remove(i);
+    }
+
+    void deleteAccount(Account a, String username, String password){
+        if(a.verifyAccount(username, password)){
+            accountList.remove(a);
+        }
+    }
+
     void addLocation(String name, String address, ArrayList<Categories> categories){
         locationList.add(new Location(name, address, categories));
     }
