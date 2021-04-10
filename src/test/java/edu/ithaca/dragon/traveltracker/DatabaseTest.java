5db3041cc1de47c41be204d2107bdd2823be8af9
Shmizzy <carnold1@ithaca.edu>
2021-04-10 17:44:31 -0400

    Added a more cohesive testing sequence. This also provides a tutorial for querying the database in the rest of the code.

--- src/test/java/edu/ithaca/dragon/traveltracker/DatabaseTest.java
+++ src/test/java/edu/ithaca/dragon/traveltracker/DatabaseTest.java
@@ -3,13 +3,51 @@ package edu.ithaca.dragon.traveltracker;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
 
     @Test
+    /*
+        Simple checking to be sure the database connects, if not it will throw an exception
+     */
     void connectionTest(){
         Connection con = Database.connect();
+    }
+
+
+    /*
+        Examples of some queries that could be useful, how to use SQL statements
+        to store and retrieve values
+     */
+    @Test
+    void queryTest() throws SQLException {
+        Connection con = Database.connect();
+
+        String sql = "SELECT * FROM accounts";
+        Statement statement = con.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+
+        //while(result.next()) will be used when we have multiple rows of values
+        //it would encapsulate the below code
+
+        String email = result.getString("email");
+        String username = result.getString("username");
+        String password = result.getString("password");
+        String permissions = result.getString("permissions");
+
+        //Just tests to make sure we get the right values from the database
+        assertEquals("admin@gmail.com", email);
+        assertEquals("admin", username);
+        assertEquals("admin123", password);
+        assertEquals("admin", permissions);
+
+
+        System.out.println(email + ", " + username +", "
+        +password + ", "+permissions);
     }
 }
