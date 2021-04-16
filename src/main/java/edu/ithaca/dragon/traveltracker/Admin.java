2a97f8e14ad636d124795cfa50e7c214bb4f8811
hoshamania <57670943+hoshamania@users.noreply.github.com>
2021-04-16 08:17:36 -0400

    Create Admin

---
+++ src/main/java/edu/ithaca/dragon/traveltracker/Admin.java
@@ -0,0 +1,24 @@
+package edu.ithaca.dragon.traveltracker;
+
+public class Admin {
+    private String username;
+    private String password;
+
+    public Admin(String username, String password){
+        if(username.length()>6){
+            this.username=username;
+            this.password = password;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid email or username");
+        }
+    }
+
+    boolean verifyAccount(String usernameIn, String passIn){
+
+        if(usernameIn.equals(username) && passIn.equals(password)){
+            return true;
+        }
+        return false;
+    }
+}
