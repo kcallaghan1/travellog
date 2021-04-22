a829ccb99c554d985175af60080e65078316719f
Danny <dxumail.dxu@gmail.com>
2021-04-21 20:44:14 -0400

    added a test to view request list

--- src/test/java/edu/ithaca/dragon/traveltracker/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/traveltracker/AdminTest.java
@@ -21,4 +21,28 @@ public class AdminTest {
         assertFalse(admin.verifyAccount("AdminUs", "password"));
         assertTrue(admin.verifyAccount("AdminUser", "password"));
     }
+
+    @Test
+    void viewLocationRequestsTest(){
+        Admin admin = new Admin("AdminUser", "password");
+        Account a = new Account("Happy", "s@s.com", "party123");
+
+        Location location1 = new Location("Waffle Frolic", "146 E State St, Ithaca, NY 14850");
+        Location location2 = new Location("Starbucks", "130 E Seneca St, Ithaca, NY 14850");
+        Location location3 = new Location("New Delhi Diamond's", "106 W Green St, Ithaca, NY 14850");
+
+        //empty list
+        assertEquals("", admin.viewLocationRequests("add"));
+        assertEquals("", admin.viewLocationRequests("remove"));
+        //adding to both lists
+        a.requestAddLocation(location1);
+        a.requestAddLocation(location2);
+        a.requestAddLocation(location3);
+        a.requestRemoveLocation(location1);
+        a.requestRemoveLocation(location2);
+        a.requestRemoveLocation(location3);
+        //Displays all the locations
+        assertEquals("Waffle Frolic, 146 E State St, Ithaca, NY 14850\nStarbuck, 130 E Seneca St, Ithaca, NY 14850\nNew Delhi Diamond's, 106 W Green St, Ithaca, NY 14850\n", admin.viewLocationRequests("add"));
+        assertEquals("Waffle Frolic, 146 E State St, Ithaca, NY 14850\nStarbuck, 130 E Seneca St, Ithaca, NY 14850\nNew Delhi Diamond's, 106 W Green St, Ithaca, NY 14850\n", admin.viewLocationRequests("remove"));
+    }
 }
